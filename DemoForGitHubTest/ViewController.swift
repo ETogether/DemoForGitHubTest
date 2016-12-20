0b2d2be35ee6e4ff4417030ef70745b706d35180
ETogether <h1949520@163.com>
2016-12-20 21:20:30 +0800

    创建UIView

---
+++ DemoForGitHubTest/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  DemoForGitHubTest
+//
+//  Created by My on 2016/12/20.
+//  Copyright © 2016年 CQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+//        self.title = "GitHub测试"
+        self.navigationItem.title = "GitHubForMyUseTest"
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
